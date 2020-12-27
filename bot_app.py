7a508945e6e820a367369831e8c7fc27dc9ff6b1
Franciszek Job <jellybean@onet,eu>
2020-12-27 19:04:36 +0100

    ep-2

--- bot_app.py
+++ bot_app.py
@@ -1,5 +1,8 @@
 import discord
 
+intents = discord.Intents.default()
+intents.members = True
+
 
 def get_token():
     with open("config.txt", "r") as config_file:
@@ -10,13 +13,25 @@ def get_token():
 
 TOKEN = get_token()
 
-client = discord.Client()
+client = discord.Client(intents=intents)
 
 
 @client.event
 async def on_message(message):
+    server_id = 792768885073117195  # id serwera
+    server = client.get_guild(server_id)
     if "czesc" in message.content:
         await message.channel.send("Witaj!")
+
+    if "!uzytkownicy" in message.content:
+        await message.channel.send("Liczba użytkowników na serwerze: " + server.member_count)
+
+
+@client.event
+async def on_member_join(member):
+    welcome_channel_id = 792775001295945769  # id kanału z powitaniami
+    await client.get_channel(welcome_channel_id).send(member.mention + " witaj na serwerze!",
+                                                      file=discord.File("assets/hello.jpg"))
 
 
 client.run(TOKEN)
